fd88ace81e2b2bce538295e9ed64e4db3df1449f
Ben Oliver <benjamin.e.oliver@gmail.com>
2017-09-25 23:43:25 +0200

    added basic vpacking, recursive nature won't play nicely with big structures

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.arangodb/arangodb-java-driver "4.2.2"]])
+                 [com.arangodb/arangodb-java-driver "4.2.2"]
+                 ;; SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
+                 ])
--- src/clj_arangodb/velocypack/core.clj
+++ src/clj_arangodb/velocypack/core.clj
@@ -1,1 +1,72 @@
-(ns clj-arangodb.velocypack.core)
+(ns clj-arangodb.velocypack.core
+  (:refer-clojure :exclude [get get-in])
+  (:require [clj-arangodb.velocypack.utils :as utils])
+  (:import com.arangodb.velocypack.VPackSlice
+           com.arangodb.velocypack.ValueType
+           com.arangodb.velocypack.VPack
+           com.arangodb.velocypack.VPackBuilder))
+
+(defn get-type [slice]
+  (.getType slice))
+
+(defn get
+  "Returns the `VPackSlice` mapped to key, not-found or nil if key not present."
+  ([slice k] (get slice k nil))
+  ([slice k not-found]
+   (let [val (.get slice (utils/normalize k))]
+     (if (.isNone val) not-found val))))
+
+(defn get-in
+  "Returns the `VPackSlice` in a nested `VPackSlice` structure,
+  where ks is a sequence of keys. Returns nil if the key
+  is not present, or the not-found value if supplied."
+  ([slice ks] (get-in slice ks nil))
+  ([slice ks not-found]
+   (let [inner-slice (reduce (fn [slice k]
+                               (.get slice (utils/normalize k))) slice ks)]
+     (if (.isNone inner-slice) not-found inner-slice))))
+
+
+(defn new-vpack-builder []
+  (new VPackBuilder))
+
+(declare build-map)
+(declare build-array)
+
+(defn vpack [xs]
+  (.slice (cond (map? xs)
+                (build-map (-> (new VPackBuilder) (.add ValueType/OBJECT)) xs)
+                (string? xs) nil
+                (nil? xs) nil
+                (utils/seqable? xs)
+                (build-array (-> (new VPackBuilder) (.add ValueType/ARRAY)) xs)
+                :else nil)))
+
+(defn build-array [builder seq]
+  (-> (reduce (fn [builder elem]
+                (cond (map? elem) (-> builder
+                                      (.add ValueType/OBJECT)
+                                      (build-map elem))
+                      (string? elem) (.add builder elem)
+                      (nil? elem) (.add builder elem)
+                      (utils/seqable? elem) (-> builder
+                                                (.add ValueType/ARRAY)
+                                                (build-array elem))
+                      :else (.add builder elem))) builder seq)
+      .close))
+
+(defn build-map
+  [builder m]
+  (as-> builder $
+    (reduce (fn [builder [k v]]
+              (cond (map? v) (-> builder
+                                 (.add (utils/normalize k) ValueType/OBJECT)
+                                 (build-map v))
+                    (string? v) (.add builder (utils/normalize k) v)
+                    (nil? v) (.add builder (utils/normalize k) nil)
+                    (utils/seqable? v) (-> builder
+                                           (.add (utils/normalize k) ValueType/ARRAY)
+                                           (build-array (seq v)))
+                    :else (.add builder (utils/normalize k) (utils/normalize v))))
+            $ m)
+    (.close $)))
