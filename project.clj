0cca2f70fe62e8c8dd284e567abc073ad3424755
Ben Oliver <benjamin.e.oliver@gmail.com>
2018-07-23 00:34:30 +0200

    updated project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beoliver/clj-arangodb "0.0.2"
+(defproject beoliver/clj-arangodb "0.0.3"
   :description "A Clojure wrapper for ArangoDB"
   :url "https://github.com/beoliver/clj-arangodb"
   :license {:name "Eclipse Public License"
