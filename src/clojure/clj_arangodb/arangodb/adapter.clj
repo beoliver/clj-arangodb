a607f5d5bfced8fc365f1a8574766e9beb1f4de8
Ben Oliver <benjamin.e.oliver@gmail.com>
2018-08-13 20:36:10 +0200

    Adapter functionality.
    Makes use of Entity interface introduced in java driver 4.7.0

--- src/clojure/clj_arangodb/arangodb/adapter.clj
+++ src/clojure/clj_arangodb/arangodb/adapter.clj
@@ -1,13 +1,30 @@
 (ns clj-arangodb.arangodb.adapter
   (:require [clj-arangodb.velocypack.core :as vpack])
-  (:import [clojure.lang
+  (:import [java.util
+            ArrayList]
+           [clojure.lang
             PersistentArrayMap
             PersistentHashMap]
            [com.arangodb.velocypack
             VPackSlice]
            [com.arangodb.entity
-            DocumentCreateEntity
+            Entity
+            MultiDocumentEntity
             BaseDocument]))
+
+(defmulti double-quote-strings class)
+(defmethod double-quote-strings String
+  [x] (str "\"" x "\""))
+(defmethod double-quote-strings clojure.lang.APersistentVector
+  [xs] (mapv double-quote-strings xs))
+(defmethod double-quote-strings clojure.lang.APersistentMap
+  [xs] (into {} (for [[k v] xs] [(double-quote-strings k) (double-quote-strings v)])))
+(defmethod double-quote-strings clojure.lang.ASeq
+  [xs] (seq (map double-quote-strings xs)))
+(defmethod double-quote-strings clojure.lang.APersistentSet
+  [xs] (set (map double-quote-strings xs)))
+(defmethod double-quote-strings :default
+  [x] x)
 
 (def ^:const entity-package-name "com.arangodb.entity")
 
@@ -18,47 +35,62 @@
 (defmulti serialize-doc class)
 (defmulti deserialize-doc class)
 
-(defmethod serialize-doc :default [o]
-  ;; we provide some basic handling.
-  ;; probably enough for most people.
-  ;; we only try to serialize clojure maps
-  (if (map? o)
-    (vpack/pack o)
-    o))
+(defmethod serialize-doc :default [o] (vpack/pack o))
 
-(defmethod deserialize-doc :default [o]
-  ;; we provide some basic handling.
-  ;; probably enough for most people.
-  (let [c (class o)]
-    (cond (= c VPackSlice) (vpack/unpack o)
-          (= c BaseDocument) (bean o)
-          :else o)))
+(defmethod deserialize-doc VPackSlice [o] (vpack/unpack o))
+(defmethod deserialize-doc BaseDocument [o] (bean o))
+(defmethod deserialize-doc :default [o] o)
 
 (defmulti from-entity class)
 
-(defn is-entity? [obj]
-  (when obj
-    (= entity-package-name (-> obj class .getPackage .getName))))
+(defn is-entity? [o]
+  (when o
+    (or (instance? Entity o)
+        (= entity-package-name (-> o class .getPackage .getName)))))
 
-(defmethod from-entity :default [obj]
-  ;; only some of the values will be entities
-  (cond (is-entity? obj)
-        ;; construct the entity recursively
-        (try (.getDeclaringClass obj)
-             ;; some entites only make sense as string
-             ;; very much a heuristic here
-             (str obj)
-             (catch java.lang.IllegalArgumentException _
-               (persistent!
-                (reduce (fn [m [k v]]
-                          (assoc! m k (from-entity v)))
-                        (transient {}) (bean obj)))))
-        ;; an array 'inside' an entity
-        ;; we only map if we know the first item is an entity
-        ;; this is because a MultiDocumententity may contain
-        ;; return values or Entities depending on the call.
-        (= java.util.ArrayList (-> obj class))
-        (cond (empty? obj) []
-              (is-entity? (.get obj 0)) (vec (map from-entity obj))
-              :else (vec obj))
-        :else obj))
+(defmethod from-entity Enum [o] (str o))
+
+(defn as-entity-vec-or-deserialized [^ArrayList o]
+  (if (= 0 (.size o))
+    []
+    (let [f (if (is-entity? (.get o 0)) from-entity deserialize-doc)
+          n (.size o)]
+      (loop [i 0
+             xs (transient [])]
+        (if (= i n)
+          (persistent! xs)
+          (recur
+           (inc i)
+           (conj! xs (f (.get o i)))))))))
+
+(defmethod from-entity MultiDocumentEntity
+  [^MultiDocumentEntity o]
+  (-> o
+      bean
+      (dissoc :documentsAndErrors)
+      (update :documents as-entity-vec-or-deserialized)
+      (update :errors as-entity-vec-or-deserialized)))
+
+(defmethod from-entity ArrayList
+  [^ArrayList o]
+  (if (= 0 (.size o))
+    []
+    (let [x (.get o 0)]
+      (if (is-entity? x)
+        (let [n (.size o)]
+          (loop [i 0
+                 xs (transient [])]
+            (if (= i n)
+              (persistent! xs)
+              (recur
+               (inc i)
+               (conj! xs (from-entity (.get o i)))))))
+        (vec o)))))
+
+(defmethod from-entity :default [o]
+  (if-not (is-entity? o)
+    o
+    (persistent!
+     (reduce (fn [m [k v]]
+               (assoc! m k (from-entity v)))
+             (transient {}) (bean o)))))
