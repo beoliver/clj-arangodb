fb81806a90eacf6263f4fd049748396fddb988e3
Ben Oliver <benjamin.e.oliver@gmail.com>
2018-08-13 20:00:33 +0200

    restructuring project to allow for additional java code
    Updated java driver to version 4.7.0

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/beoliver/clj-arangodb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.arangodb/arangodb-java-driver "4.6.0"]
+                 [com.arangodb/arangodb-java-driver "4.7.0"]
                  [org.slf4j/slf4j-simple "1.7.25"]])
