8d5ec81aebcefc11f134954418052ee26dd22104
Ben Oliver <benjamin.e.oliver@gmail.com>
2018-08-13 20:09:30 +0200

    Better handling for option building.
    [Fixes #11]
    Objects that are passed as values in a map are not evaluated - this
    avoids the runtimeException. Additional meta data is added so that
    we do not encounter reflection warnings.

--- src/clojure/clj_arangodb/arangodb/options.clj
+++ src/clojure/clj_arangodb/arangodb/options.clj
@@ -1,35 +1,24 @@
-(ns clj-arangodb.arangodb.options
-  (:import com.arangodb.model.CollectionCreateOptions
-           com.arangodb.entity.CollectionType
-           com.arangodb.model.DocumentCreateOptions
-           com.arangodb.model.DocumentReadOptions
-           com.arangodb.model.DocumentReplaceOptions
-           com.arangodb.model.DocumentUpdateOptions
-           com.arangodb.model.DocumentDeleteOptions
-           com.arangodb.model.DocumentExistsOptions
-           com.arangodb.model.DocumentReadOptions
-           com.arangodb.model.GraphCreateOptions
-           com.arangodb.model.GeoIndexOptions
-           com.arangodb.model.VertexCollectionCreateOptions
-           com.arangodb.model.VertexCreateOptions
-           com.arangodb.model.VertexDeleteOptions
-           com.arangodb.model.VertexReplaceOptions
-           com.arangodb.model.VertexUpdateOptions
-           com.arangodb.model.EdgeCreateOptions
-           com.arangodb.model.EdgeDeleteOptions
-           com.arangodb.model.EdgeReplaceOptions
-           com.arangodb.model.EdgeUpdateOptions
-           com.arangodb.model.AqlQueryOptions
-           com.arangodb.ArangoDB$Builder))
+(ns clj-arangodb.arangodb.options)
 
-(defn fn-builder
-  "given a map entry create a function that wraps a method call
-  using the method with the same name as the keyword.
-  Arguments single multiple arguments should be in a seq"
-  [[method-key args]]
+(defn class->symbol [^Class c]
+  (symbol (.substring (.toString c) 6)))
+
+(defn- fn-builder
+  "given a map entry that represents a method call where the key (keyword)
+  is the method name and the `val` are args (modulo the implicit object)
+  returns a partial function that takes a single `object` argument."
+  ;; to ensure that objects can be passed as args, we need to create a
+  ;; partial function so that we do not need to eval any args directly!
+  ;; Arguments single multiple arguments should be in a seq
+  [class-symbol [method-key args]]
   (let [method-name (symbol (name method-key))
-        args (flatten (list args))]
-    (eval `(fn [x#] (. x# ~method-name ~@args)))))
+        args (flatten (list args))
+        params (vec (repeatedly (inc (count args)) #(gensym)))
+        ;; add meta to the object so that we dont get reflection warnings!
+        obj-param (with-meta (peek params) {:tag class-symbol})
+        arg-params (pop params)
+        f (eval `(fn ~params (. ~obj-param ~method-name ~@arg-params)))]
+    (apply partial f args)))
 
 (defn build
   "given a class name and map of options create a sequence of function calls
@@ -37,5 +26,6 @@
   [class options]
   (if (map? options)
     (reduce (fn [acc f]
-              (f acc)) (eval `(new ~class)) (map fn-builder options))
+              (f acc)) (eval `(new ~class))
+            (map (partial fn-builder (class->symbol class)) options))
     options))
