ae3d42f56a6dd9335000049d83764bd4a4536040
tuddman <scott.tudd@gmail.com>
2020-07-18 19:38:45 -0600

    updates dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject beoliver/clj-arangodb "0.0.8"
+(defproject beoliver/clj-arangodb "0.1.0-SNAPSHOT"
   :description "A Clojure wrapper for ArangoDB"
   :url "https://github.com/beoliver/clj-arangodb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.arangodb/arangodb-java-driver "4.7.0"]
-                 [org.slf4j/slf4j-simple "1.7.25"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.arangodb/arangodb-java-driver "6.6.3"]
+                 [org.slf4j/slf4j-simple "1.7.30"]])
